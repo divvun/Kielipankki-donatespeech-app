3dcb64d3b9ea40cf92f932345c4a02f6084e9235
Børre Gaup <albbas@gmail.com>
2026-03-05 11:56:06 +0100

    Refactor translation hook string accessor

--- src/hooks/useTranslation.ts
+++ src/hooks/useTranslation.ts
@@ -9,11 +9,16 @@ import type { FluentVariable } from "@fluent/bundle";
 export function useTranslation() {
   const { l10n } = useLocalization();
 
+  const getString = (
+    id: string,
+    args?: Record<string, FluentVariable> | null,
+  ) => {
+    const message = l10n.getString(id, args);
+    // Fluent returns the ID if translation is missing
+    return message || id;
+  };
+
   return {
-    getString: (id: string, args?: Record<string, FluentVariable> | null) => {
-      const message = l10n.getString(id, args);
-      // Fluent returns the ID if translation is missing
-      return message || id;
-    },
+    getString,
   };
 }
